32ee3000f580c3e3b77dcc14d0505b5af4bb5619
alesslongaretti <alesslongaretti@gmail.com>
2020-04-30 21:38:54 -0400

    tested fail and it passed - MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -12,21 +12,27 @@ function succeed(item) {
   if(newEnhancement === 20 ) {
     return {...item}
   } 
-  else if (newEnhancement > 20) {
-    return null
-  } else if (newEnhancement < 20) {
+   else if (newEnhancement < 20) {
     return {...item, enhancement: newEnhancement + 1}
   }
 };
 
 function fail(item) {
-  return { ...item };
-}
+  const newEnhancement = item.enhancement;
+
+  if(newEnhancement > 16){
+    return {...item, durability: item.durability - 10, enhancement: newEnhancement - 1}
+  } else if (newEnhancement < 15) {
+    return {...item, durability: item.durability - 5}
+  } else{
+    return {...item, durability: item.durability - 10}
+  }
+};
 
 function repair(item) {
   const newItem = { name: item.name ,durability: 100, enhancement: item.enhancement};
   return newItem;
-}
+};
 
 function get(item) {
   return { ...item };
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -14,4 +14,16 @@ describe('testing enhancer', () => {
             expect(succeed({ name: 'alessandra', durability: 11, enhancement:19})).toEqual({ name: 'alessandra', durability: 11, enhancement:20})
         })
     })
+    describe('fail()', () => {
+        it('if the items enhancement is less than 15, durability of item is decreased by 5', () => {
+            expect(fail({name: 'alessandra', durability:50, enhancement:14})).toEqual({name: 'alessandra', durability:45, enhancement:14})
+        })
+        it('if the items enhancement is 15 or more, durability is decreased by 10', () => {
+            expect(fail({enhancement:15, durability:50 })).toEqual({ enhancement: 15,durability:40})
+
+        })
+        it('if the items level is greater than 16, the enhancement decreases by 1', () => {
+            expect(fail({name: 'alessandra',durability:20, enhancement:17})).toEqual({name: 'alessandra',durability:10, enhancement:16})
+        })
+    })
 })
